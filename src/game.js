91d901392a7f06f0a56f71e2bcea14a436e43756
ShizukuIchi <shizukuichi@gmail.com>
2018-10-01 11:47:00 +0800

    server logger

--- src/game.js
+++ src/game.js
@@ -140,6 +140,7 @@ function draw() {
         setTimeout(function() {
           score.innerHTML += ' 鍵入空白再來一把';
         }, 1000);
+        fetch(`${location.href}score?score=${scoreCount}`)
         characterSound.play()
         status = 'stopped'
         noLoop(); // stop loop
