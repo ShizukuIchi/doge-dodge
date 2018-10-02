160ca5f52f3a7c53ce9474d438cff86d12b52835
ShizukuIchi <shizukuichi@gmail.com>
2018-10-02 10:58:31 +0800

    uglify

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const serve = require('koa-static');
 const Koa = require('koa');
+const compress = require('koa-compress')
 const app = new Koa();
 const port = process.env.PORT || 3000
+
+const options = { threshold: 2048 };
+app.use(compress(options));
 
 app.use(async (ctx, next) => {
   await next();
